bc0547005ae037d7bd9e8b71faff634abf74b7e3
Robin Anderson <robin@rbanderson.co.uk>
2017-01-30 20:34:31 +0000

    handle paths

--- source/image_uploader.py
+++ source/image_uploader.py
@@ -1,10 +1,12 @@
 import storage
 import messagebus
+import os
 
 class SubscribingUploader:
   def __init__(self):
-    self._uploader = storage.S3Storage(bucketName = "rba-picam")
+    self._uploader = storage.S3Storage(bucketName = "rba-picam", s3Folder="images")
     self._serialiser = messagebus.BusMessageSerialiser()
+    self._s3Folder = s3Folder
 
   def start(self):
     self._subscriber = messagebus.RedisbusSubscriber("localhost", 6379)
@@ -14,11 +16,12 @@ class SubscribingUploader:
     self._subscriber.close()
 
   def _handler(self, rawMessage):
-    print(rawMessage)
     message = self._serialiser.deserialise(rawMessage)
-    if message.messageType == "image-captured":
+    if message.messageType == "image-captured":      
       filePath = message.payload["fileName"]
       print("Uploading: " + str(filePath))
-      self._uploader.move(filePath, filePath)
+      localFolder, fileName = os.path.split(filePath)
+      targetPath = os.path.join(self._s3Folder, fileName)
+      self._uploader.move(filePath, targetPath)
     else:
       print("Ignoring: " + message.messageType)
